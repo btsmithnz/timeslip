02e7d7cde7ee1315b8a2dfe69d09ce08b452368c
Ben Smith <btsmith.nz@gmail.com>
2026-03-14 15:42:10 +1100

    Fix imports

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,7 +8,7 @@ import { ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import "../global.css";
+import "../../global.css";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { authClient } from "@/lib/auth-client";
